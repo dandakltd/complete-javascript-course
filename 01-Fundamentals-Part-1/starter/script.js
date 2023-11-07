539e81993a74d21b2faa9cce42c1f32bce7d816d
Dilshan Perera <dilshansanjeewaperera@gmail.com>
2023-11-07 18:59:43 -0500

    added new notes about vars and tested some naming conventions

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,21 @@
 let js = "amazing";
-if (js === "amazing") alert = ("JS is fun!");
 console.log(40 + 8 + 23 - 10);
+// console.log('Jonas');
+console.log(23);
+
+let firstname = "Matilda";
+let lastName = "perera";
+let middle_name = "sanjeewa";
+console.log(firstname);
+console.log(lastName);
+console.log(middle_name);
+console.log(firstname);
+
+// A variable cannot start with a number
+// A variable cannot be seperated by &
+// "new, function" cannot be used as variables you can instead modify
+// "name" is a reserved keyword so better not to use it as a variable name
+// Variables cannot be starting with an uppuercase  letter.
+// variables which will not get changes can only be starting with a uppercase
+    // eg : let PI = 3.1415; (it has to be real constant)
+
